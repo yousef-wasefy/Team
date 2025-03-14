de046a596d147ef7e4fb6c81f60e1bc68a956bf9
Youssef <yousefalwasefy@gmail.com>
2025-03-15 00:03:13 +0200

    test

--- team.cpp
+++ team.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     cout << "I'm youssef" << endl;
-    //test
-    cout<<" I am Nour "<<endl; 
-    
+    // test
+    cout << " I am Nour " << endl;
+    cout << "I'm Joo" << endl;
 }
